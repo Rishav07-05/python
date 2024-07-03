c95dc3e2bf9df4be83d02db4fb2ca6c0486e9841
Rishav Kumar <151399837+Rishav07-05@users.noreply.github.com>
2024-07-03 22:23:22 +0530

    Add files via upload

---
+++ 01_first.py
@@ -0,0 +1,65 @@
+# print("\nNamaste Duniya :) \n")
+
+# Printing jokes
+
+# import pyjokes
+
+# Myjokes = pyjokes.get_joke()
+# print(Myjokes)
+
+
+# Practice Set 
+
+
+# print("\nThere we go with the poem part: \n")
+
+# print(''' Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+
+# Then the trav'ller in the dark,
+# Thanks you for your tiny spark,
+# He could not see which way to go,
+# If you did not twinkle so.
+
+# In the dark blue sky you keep,
+# And often thro' my curtains peep,
+# For you never shut your eye,
+# Till the sun is in the sky.
+
+# 'Tis your bright and tiny spark,
+# Lights the trav'ller in the dark:
+# Tho' I know not what you are,
+# Twinkle, twinkle, little star.
+#        ''')
+
+
+
+
+# using  pyttsx3 for text to speech 
+
+# import pyttsx3
+# jarvis = pyttsx3.init()
+# voices = jarvis.getProperty('voices')
+# jarvis.setProperty('voice', voices[1].id)   
+# jarvis.say("Hey Rishav how are you")
+# jarvis.runAndWait()
+
+
+
+# import os
+
+# path = '/Games'
+
+# contents = os.listdir(path)
+
+# for item in contents:
+#     print(item)
+
+
