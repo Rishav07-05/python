983552d9e15fc44cc23a96538af8403b81bc2d7a
Rishav Kumar <151399837+Rishav07-05@users.noreply.github.com>
2024-07-03 22:25:26 +0530

    Add files via upload

---
+++ 09_practiceDict.py
@@ -0,0 +1,60 @@
+# Q1. Prac
+
+# words = {
+#     "Maffi" : "Sorry",
+#     "Ruko" : "Wait",
+#     "Kidhar" : "Where",
+#     "Jagah" : "Place",
+#     "Billi" : "Cat"
+# }
+
+# word = input("Enter the word you want meaning of: ")
+# print(words[word])
+
+
+# Q2. Prac
+
+# s = set()
+
+# user = int(input("Enter the first number: "))
+# s.add(user)
+# user = int(input("Enter the second number: "))
+# s.add(user)
+# user = int(input("Enter the third number: "))
+# s.add(user)
+# user = int(input("Enter the fourth number: "))
+# s.add(user)
+# user = int(input("Enter the fifth number: "))
+# s.add(user)
+# user = int(input("Enter the sixth number: "))
+# s.add(user)
+# user = int(input("Enter the seventh number: "))
+# s.add(user)
+# user = int(input("Enter the eighth number: "))
+# s.add(user)
+
+# print(s)
+
+
+
+# Q3. Pracs
+
+# dict = {}
+
+# name = input("Enter friends name: ")
+# lang = input("Enter language name: ")
+# dict.update({name: lang})
+
+# name = input("Enter friends name: ")
+# lang = input("Enter language name: ")
+# dict.update({name: lang})
+
+# name = input("Enter friends name: ")
+# lang = input("Enter language name: ")
+# dict.update({name: lang})
+
+# name = input("Enter friends name: ")
+# lang = input("Enter language name: ")
+# dict.update({name: lang})
+
+# print(dict)
