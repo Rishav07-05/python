93f29d85f8d6b4acd9a2ba6470acde93f02c8451
Rishav Kumar <151399837+Rishav07-05@users.noreply.github.com>
2024-07-03 22:23:50 +0530

    Add files via upload

---
+++ 03_strings.py
@@ -0,0 +1,58 @@
+# a = "Rishav"
+# A sequence of characters is known as ===> Strings
+# Strings are immutable
+# sortname = a[0:4] #sorting the String
+# char = a[0]
+# print(sortname)
+# print(char)
+
+
+
+# slicing of string 
+
+# char = a[0:5:2]
+
+# print("Sliced Name: ",char)
+
+
+# Inbuilt functions in string 
+
+# name = "rishav"
+# print("Length : ",len(name))
+# print(name.endswith("av"))
+# print(name.startswith("lsat"))
+# print(name.capitalize())
+# print(name.lower())
+# print(name.upper())
+# print(name.find("s"))
+# print(name.replace("s" , "z"))
+
+
+#Practice Questions 
+
+# 1.
+# name = str(input("Enter You want to enter: "))
+# print("Good",name)
+
+
+# 2.
+# name = str(input(": "))
+# if(str == '  '):
+#     print("Double Space")
+# else:
+#     print("No Space")
+
+
+# 3.
+# letter =  '''
+#     Dear <|Name|> 
+#     you are selected
+#     <|Date|>
+# '''
+# n = str(input("Enter your name: "))
+# print(letter.replace("<|Name|>" , n).replace("<|Date|>" ,  "05-FEB-2026"))
+
+
+# 4.
+# name = "Rishav  hey"
+# print(name.find("  "), name.replace("  " , "   "))
