72cbbbea5c7da5e1aa59454febeb123cfd8bebb0
Rishav Kumar <151399837+Rishav07-05@users.noreply.github.com>
2024-07-03 22:24:41 +0530

    Add files via upload

---
+++ 06_practiceList.py
@@ -0,0 +1,45 @@
+# Q1. prac 
+
+# fruits = []
+# a = (input("Enter first fruit: "))
+# fruits.append(a)
+# b = (input("Enter second fruit: "))
+# fruits.append(b)
+# c = (input("Enter third fruit: "))
+# fruits.append(c)
+# d = (input("Enter fourth fruit: "))
+# fruits.append(d)
+# e = (input("Enter fifth fruit: "))
+# fruits.append(e)
+# f = (input("Enter sixth fruit: "))
+# fruits.append(f)
+# g = (input("Enter seventh fruit: "))
+# fruits.append(g)
+
+# print(fruits)
+
+
+# Q2. prac 
+
+# Marks = []
+# a = (input("Enter first Mark: "))
+# Marks.append(a)
+# b = (input("Enter second Mark: "))
+# Marks.append(b)
+# c = (input("Enter third Mark: "))
+# Marks.append(c)
+# d = (input("Enter fourth Mark: "))
+# Marks.append(d)
+# e = (input("Enter fifth Mark: "))
+# Marks.append(e)
+# f = (input("Enter sixth Mark: "))
+# Marks.append(f)
+
+# Marks.sort()
+# print(Marks)
+
+
+# Q3. prac
+
+num = [1,4,5,7]
+print(sum(num)) # ***SUM*** is a built in function 
