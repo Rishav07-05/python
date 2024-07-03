15a2b3ff30039950ab81604c902e79ffde874e80
Rishav Kumar <151399837+Rishav07-05@users.noreply.github.com>
2024-07-03 22:23:38 +0530

    Add files via upload

---
+++ 02_variable.py
@@ -0,0 +1,83 @@
+# variable mainly act as a container 
+
+# a = 30 # interger
+# b = "Rishav" # string
+# c = True # bool 
+# d = 2.22 #float
+# e = None # none type of variable 
+
+
+# Arithmetic operators 
+
+# a = 7
+# b = 4
+# print("Sum is before: ",a+b)
+# b+=3
+# print("Sum is after: ",a+b) 
+# print(a>b) 
+
+
+# e = True or False
+# print("True or False : ",e)
+
+
+# typecasting
+
+# a = 5
+# b = str(a)
+# print(b)
+# print(type(b))
+
+
+# take input 
+
+
+# a = int(input("Enter a number1: "))
+# b = int(input("Enter a number2: "))
+# print("Sum of both is : " , a + b)
+
+
+# Practice Questions 
+
+
+# 1. Sum of two number
+
+# a = int(input("Enter Your input: "))
+# b = int(input("Enter Your input: "))
+# print("sum is : ", a+b)
+
+
+#2. Find remainder 
+
+# a = int(input("Enter your input: "))
+# z = int(input("Enter number to find remainder: "))
+# print("The remainder is: ", a%z)
+
+
+# 3. check datatypes
+
+# a = int(input("Enter any number: "))
+# checkType = type(a)
+# print("Your Datatypes: ", checkType)
+
+
+#4. check greater 
+
+# a = 34
+# b = 80
+# print("B is a greater number " , b > a)
+
+
+# 5. Average of numbers
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# average = (a+b)/2
+# print("The average of two numbers are: ",average)
+
+
+# 6. Calculate the square 
+
+# a = int(input("Enter the first number: "))
+# square = a*a
+# print("The square of ", a ," is : ",square)
