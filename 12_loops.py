6bac8b83ac06b2aded734a4fb7dce3b48f177631
Rishav⚔️ <151399837+Rishav07-05@users.noreply.github.com>
2024-07-18 19:48:45 +0530

    Add files via upload

---
+++ 12_loops.py
@@ -0,0 +1,47 @@
+# For loops counting scenario 
+# for i in range (1,6):
+#     print(i)
+
+# While loops counting scenario
+
+# i = 1
+# while(i <= 5):
+#     print(i)
+#     i+=1
+
+
+#  looping in a list using while loop
+
+# list = [1, "rishav" , "batkumar" , "rani" , "5" , False]
+# i = 0
+# while(i < len(list)):
+#     print(list[i])
+#     i+=1
+
+
+# looping in a list using for loop 
+# list = [1, "rishav" , "batkumar" , "rani" , "5" , False]
+# length = len(list)
+# for i in range(0,length):
+#     print(list[i])
+
+
+# looping in a tuple using for loop 
+# t = (4 , 6 , 764 , 293)
+# for i in t:
+#     print(i)
+
+
+# Iterating through strings using for loop 
+# s = "Rishav"
+# for i in s:
+#     print(i)
+
+
+# loop with else statement
+
+l = [1,4,7,3]
+for item in l:
+    print(item)
+else:
+    print("Done")
