f2647a4a2e5ae3d65a2ad5faf77f11a3c29351cd
Rishav⚔️ <151399837+Rishav07-05@users.noreply.github.com>
2024-07-18 19:49:02 +0530

    Add files via upload

---
+++ 13_ExitStatements.py
@@ -0,0 +1,13 @@
+# Break Statement => Exits the loop 
+# for i in range(100):
+#     if(i == 69):
+#         break
+#     print(i)
+
+# continue statement => To skip the iteration
+for i in range(10):
+    if(i == 5 or i == 6):
+        continue
+    print(i)
+
+# Pass is a null statement 
