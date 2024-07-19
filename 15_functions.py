dde3b95271f08e565e55e8d7bbd14a3efc18f8e3
Rishav⚔️ <151399837+Rishav07-05@users.noreply.github.com>
2024-07-19 16:08:37 +0530

    Add files via upload

---
+++ 15_functions.py
@@ -0,0 +1,34 @@
+# def avg():
+#     a = int(input("Entter the number: "))
+#     b = int(input("Entter the number: "))
+#     c = int(input("Entter the number: "))
+
+#     average = (a+b+c)/3
+#     print(average)
+
+# avg() 
+
+# def greet(name, ending): # Function with arguments
+#     print("Good Night " + name ,end="")
+#     print(ending)
+
+# greet("Rishav , ", "Thanks")
+
+
+# Return statement 
+# def greet(name, ending): 
+#     print("Good Night " + name ,end="")
+#     print(ending)
+#     return "Okay"
+
+# a = greet("Rishav , ", "Thanks")
+# print(a)
+
+
+# Default Arguments
+
+# def greet(name , ending = "Thanks"): # This is default argument
+#     print("good day " + name)
+#     print(ending)
+
+# greet("Rishav" , "OK")
