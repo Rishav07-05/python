bc6468e7fbfbd9d2d34021b4ab0b5aabbd05bccd
Rishav⚔️ <151399837+Rishav07-05@users.noreply.github.com>
2024-07-20 12:05:49 +0530

    Add files via upload

---
+++ 21_classes.py
@@ -0,0 +1,15 @@
+# class Employee:
+#     language = "Go" # this is a class attribute 
+#     salary = 19000000
+
+# rishav = Employee()
+# rishav.name = "Rishav Robinson" # this is an instance attribute
+# print(rishav.name, rishav.salary, rishav.language)
+
+
+# ram = Employee()
+# ram.name = "Ramu Kaka"
+# print(ram.name, ram.salary, ram.language)
+
+
+# Here name is instance attribute and salary and languages are class attributes as they directly belongs to the class
